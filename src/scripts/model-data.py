b53ea6166c9368a68adeb674f17d06293048a9d6
Nikki Phach <59062440+nphach@users.noreply.github.com>
2025-01-30 20:13:53 -0500

    update nonparallel data in model-data

--- src/scripts/model-data.py
+++ src/scripts/model-data.py
@@ -3,32 +3,45 @@ import random
 import math
 import json
 import html
+from nltk.corpus import wordnet as wn
 
-jmd_fp = "/JMdict_e"
-train_fp = ""
-cv_fp = ""
-test_fp = ""
+jmd_fp = "/Users/nikki/Desktop/kotoba tag/src/data/model/JMdict_e"
+train_fp = "train_2.json"
+cv_fp = "cv_2.json"
+test_fp = "test_2.json"
+
+def get_antonyms(word):
+    antonyms = set()
+    for syn in wn.synsets(word):
+        for lemma in syn.lemmas():
+            for antonym in lemma.antonyms():
+                antonyms.add(antonym.name())
+    return list(antonyms)
+
+def is_simple(gloss):
+    return len(gloss.split()) == 1
 
 with open(jmd_fp, 'r') as f:
     soup = BeautifulSoup(f, 'lxml-xml')
 
 out = []
-saved_gloss = None
+all_glosses = []
 
 for e in soup.find_all('entry'):
     for s in e.find_all('sense'):
         g = [html.unescape(x.string.strip()) for x in s.find_all('gloss')]
-        
-        if (len(g) == 1):
-            if (saved_gloss == None):
-                saved_gloss = g
-            elif (g != saved_gloss):
-                out.append([saved_gloss + g, False])
-                saved_gloss = None
-        elif (len(g) > 1):
+        all_glosses.extend(g)
+
+        if (len(g) > 1):
             for i in range(len(g) - 1):
                 for j in range(i + 1, len(g)):
                     out.append([[g[i], g[j]], True])
+
+for gloss in all_glosses:
+    if is_simple(gloss):
+        antonyms = get_antonyms(gloss)
+        for antonym in antonyms:
+            out.append([[gloss, antonym], False])
 
 random.shuffle(out)
 l = len(out)
