f6393eb3df57695f08e6a0000fbd38e482f6adf3
Nikki Phach <59062440+nphach@users.noreply.github.com>
2025-02-10 11:57:43 -0500

    update model training strategy

--- src/scripts/model-data.py
+++ src/scripts/model-data.py
@@ -5,80 +5,82 @@ import json
 import html
 from nltk.corpus import wordnet as wn
 
-jmd_fp = "/Users/nikki/Desktop/kotoba tag/src/data/model/JMdict_e"
-train_fp = "src/data/model/train.json"
-cv_fp = "src/data/model/cv.json"
-test_fp = "src/data/model/test.json"
+if __name__ == "__main__":
+    jmd_fp = "/Users/nikki/Desktop/kotoba tag/src/data/model/JMdict_e"
+    train_fp = "src/data/model/train.json"
+    cv_fp = "src/data/model/cv.json"
+    test_fp = "src/data/model/test.json"
 
-def get_antonyms(word):
-    antonyms = set()
-    for syn in wn.synsets(word):
-        for lemma in syn.lemmas():
-            for antonym in lemma.antonyms():
-                antonyms.add(antonym.name())
-    return list(antonyms)
+    def get_antonyms(word):
+        antonyms = set()
+        for syn in wn.synsets(word):
+            for lemma in syn.lemmas():
+                for antonym in lemma.antonyms():
+                    antonyms.add(antonym.name())
+        return list(antonyms)
 
-def get_synonyms(word):
-    synonym = set()
-    for syn in wn.synsets(word):
-        for lemma in syn.lemmas():
-            for synonym in lemma.similar_tos():
-                synonym.add(synonym.name())
-    return list(synonym)
+    def get_synonyms(word):
+        synonyms = set()
+        for syn in wn.synsets(word):
+            for lemma in syn.lemmas():
+                for synonym in lemma.similar_tos():
+                    synonyms.add(synonym.name())
+                for synonym in lemma.also_sees():
+                    synonyms.add(synonym.name())
+        return list(synonyms)
 
-def is_simple(gloss):
-    return len(gloss.split()) == 1
+    def is_simple(gloss):
+        return len(gloss.split()) == 1
 
-with open(jmd_fp, 'r') as f:
-    soup = BeautifulSoup(f, 'lxml-xml')
+    with open(jmd_fp, 'r') as f:
+        soup = BeautifulSoup(f, 'lxml-xml')
 
-out = []
-saved_gloss = None
-all_glosses = []
+    out = []
+    saved_gloss = None
+    all_glosses = []
 
-# parallel glosses
-for e in soup.find_all('entry'):
-    for s in e.find_all('sense'):
-        g = [html.unescape(x.string.strip()) for x in s.find_all('gloss')]
-        all_glosses.extend(g)
+    # parallel glosses
+    for e in soup.find_all('entry'):
+        for s in e.find_all('sense'):
+            g = [html.unescape(x.string.strip()) for x in s.find_all('gloss')]
+            all_glosses.extend(g)
 
-        if (len(g) == 1):
-            if (saved_gloss == None):
-                saved_gloss = g
-            elif (g != saved_gloss):
-                out.append([saved_gloss + g, False])
-                saved_gloss = None
-        elif (len(g) > 1):
-            for i in range(len(g) - 1):
-                for j in range(i, len(g)):
-                    out.append([[g[i], g[j]], True])
+            if (len(g) == 1):
+                if (saved_gloss == None):
+                    saved_gloss = g
+                elif (g != saved_gloss):
+                    out.append([saved_gloss + g, False])
+                    saved_gloss = None
+            elif (len(g) > 1):
+                for i in range(len(g) - 1):
+                    for j in range(i, len(g)):
+                        out.append([[g[i], g[j]], True])
 
-# antonym/ synonym pairs
-for gloss in all_glosses:
-    if is_simple(gloss):
-        antonyms = get_antonyms(gloss)
-        for antonym in antonyms:
-            out.append([[gloss, antonym], False])
-        synonyms = get_synonyms(gloss)
-        for synonym in synonyms:
-            print([gloss, synonym])
-            out.append([[gloss, synonym], True])
+    # antonym/ synonym pairs
+    for gloss in all_glosses:
+        if is_simple(gloss):
+            antonyms = get_antonyms(gloss)
+            for antonym in antonyms:
+                out.append([[gloss, antonym], False])
+            synonyms = get_synonyms(gloss)
+            for synonym in synonyms:
+                out.append([[gloss, synonym], True])
 
-# random negatives
-for gloss in all_glosses:
-    rand = random.choice(all_glosses)
-    if gloss != rand:
-        out.append([[gloss, rand], False])
+    # random negatives
+    for gloss in all_glosses:
+        rand = random.choice(all_glosses)
+        if gloss != rand:
+            out.append([[gloss, rand], False])
 
-random.shuffle(out)
-l = len(out)
-train = out[0:math.floor(l * 0.8)]
-cv = out[math.floor(l * 0.8): math.floor(l * 0.9)]
-test = out[math.floor(l * 0.9):l]
+    random.shuffle(out)
+    l = len(out)
+    train = out[0:math.floor(l * 0.8)]
+    cv = out[math.floor(l * 0.8): math.floor(l * 0.9)]
+    test = out[math.floor(l * 0.9):l]
 
-with open(train_fp, "w") as f:
-    json.dump(train, f, ensure_ascii=False, indent=2)
-with open(cv_fp, "w") as f:
-    json.dump(cv, f, ensure_ascii=False, indent=2)
-with open(test_fp, "w") as f:
-    json.dump(test, f, ensure_ascii=False, indent=2)
+    with open(train_fp, "w") as f:
+        json.dump(train, f, ensure_ascii=False, indent=2)
+    with open(cv_fp, "w") as f:
+        json.dump(cv, f, ensure_ascii=False, indent=2)
+    with open(test_fp, "w") as f:
+        json.dump(test, f, ensure_ascii=False, indent=2)
