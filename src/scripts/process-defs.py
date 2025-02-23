91c431ee5747abb17b38dfca7fd5bfc041a93694
Nikki Phach <59062440+nphach@users.noreply.github.com>
2025-02-23 16:09:21 -0500

    further process defs in work bank

--- src/scripts/process-defs.py
+++ src/scripts/process-defs.py
@@ -8,8 +8,12 @@ def process_definitions(data):
         definitions = entry["def"]
         seen = set(definitions)
         new_terms = []
+        terms_to_remove = set()
         
         for d in definitions:
+            if '(' in d or '-' in d:
+                terms_to_remove.add(d)
+
             if '(' in d:
                 processed = d.split('(')[0].strip()
                 if processed and processed not in seen:
@@ -21,6 +25,7 @@ def process_definitions(data):
                     new_terms.append(processed)
                     seen.add(processed)
         
+        entry["def"] = [d for d in entry["def"] if d not in terms_to_remove]
         entry["def"].extend(new_terms)
     
     return data
